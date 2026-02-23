9c09ec9092db2a1791ccbdaac69ab4dc0b3abb84
Himanshu <er.himanshuk.00@gmail.com>
2026-02-23 14:46:56 +0530

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,32 @@ function App() {
   }
   return (
     <>
-    <p>Hello this is a counter project</p>
-    <h1>Counter value: {counter}</h1>
-    <button onClick={addValue}>Add value </button><br />
-  <button onClick={ removeValue}>Remove value </button>
+  <div className="bg-green-900 text-amber-100 p-8 rounded-2xl shadow-xl text-center w-80">
+    <p className="text-lg font-semibold mb-4">
+      Hello this is a counter project
+    </p>
+
+    <h1 className="text-3xl font-bold mb-6">
+      Counter value: {counter}
+    </h1>
+
+    <div className="flex justify-center gap-4">
+      <button
+        onClick={addValue}
+        className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition"
+      >
+        Add Value
+      </button>
+
+      <button
+        onClick={removeValue}
+        className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition"
+      >
+        Remove Value
+      </button>
+    </div>
+  </div>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div className='flex items-center justify-center min-h-screen bg-amber-950'>
     <App />
+    </div>
   </StrictMode>,
 )
